dc1e4249e6e1e53e452266b934583ed67a98e156
WindWood <windwood@gmail.com>
2015-02-14 13:56:41 +0800

    Book entry in list

--- app/src/main/java/cn/windwood/app/douban/data/Book.java
+++ app/src/main/java/cn/windwood/app/douban/data/Book.java
@@ -4,9 +4,9 @@ package cn.windwood.app.douban.data;
  * Created by WindWood on 2015/2/8.
  */
 public class Book {
-    private String isbn;
-    private String title;
-    private boolean favorite;
+    public String isbn;
+    public String title;
+    public boolean favorite;
 
     public Book(String isbn, String title, boolean favorite) {
         this.isbn = isbn;
@@ -14,27 +14,4 @@ public class Book {
         this.favorite = favorite;
     }
 
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isFavorite() {
-        return favorite;
-    }
-
-    public void setFavorite(boolean favorite) {
-        this.favorite = favorite;
-    }
 }
